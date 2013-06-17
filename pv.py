2603e664d11d24025a7043146a979cfd16bdf9b5
Ben Shen <benqingshen@gmail.com>
2013-06-17 17:16:19 -0500

    Update pv

--- pv.py
+++ pv.py
@@ -1,3 +1,7 @@
+# I recommend this:
+import sys
+sys.argv
+# A useful terminology here is called "unpack"
 
 r = float(raw_input('rate?'))
 n = int(raw_input('number of pmt?'))
@@ -6,3 +10,13 @@ fv = float(raw_input('fv?'))
 cpmt = pmt / (1+r) * (1 - 1 / (1+r)**n) / (1 - 1/(1+r))
 pv = cpmt + fv / (1+r)**n
 print pv
+
+# define a function
+# http://docs.python.org/2.7/tutorial/controlflow.html#defining-functions
+def PresentValue(rate, nper, pmt, fv=0):
+  # Your logic here
+	pv = 0
+	return pv
+
+# Then run the function
+pv = PresentValue(r, n, pmt, fv)

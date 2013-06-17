4e7648866e590dbdc011852e8e47b60149f885ed
xchenmeow <chenxy12345@yahoo.com>
2013-06-17 17:23:29 -0500

    pv.py

--- pv.py
+++ pv.py
@@ -3,20 +3,17 @@ import sys
 sys.argv
 # A useful terminology here is called "unpack"
 
-r = float(raw_input('rate?'))
-n = int(raw_input('number of pmt?'))
-pmt = float(raw_input('pmt?'))
-fv = float(raw_input('fv?'))
-cpmt = pmt / (1+r) * (1 - 1 / (1+r)**n) / (1 - 1/(1+r))
-pv = cpmt + fv / (1+r)**n
-print pv
-
 # define a function
 # http://docs.python.org/2.7/tutorial/controlflow.html#defining-functions
-def PresentValue(rate, nper, pmt, fv=0):
-  # Your logic here
-	pv = 0
+def PresentValue(r, n, pmt, fv):
+	cpmt = pmt / (1+r) * (1 - 1 / (1+r)**n) / (1 - 1/(1+r))
+	pv = cpmt + fv / (1+r)**n
 	return pv
 
+rate = float(sys.argv[1])
+nper = int(sys.argv[2])
+pmt = float(sys.argv[3])
+fv = float(sys.argv[4])
 # Then run the function
-pv = PresentValue(r, n, pmt, fv)
+pv = PresentValue(rate, nper, pmt, fv)
+print pv

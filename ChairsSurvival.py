58519c36004bc1fde90f4394025963fa735ba27e
benqingshen <benqingshen@gmail.com>
2013-07-02 19:26:45 -0500

    Benqing's change of ChairsSurvival.py

--- ChairsSurvival.py
+++ ChairsSurvival.py
@@ -1,20 +1,33 @@
 def ChairsSurvival(N):
-	chairs = []
-	for i in range(0,N):
-		chairs.append(1)
-	flag = True
-	while True:
-		for i in range(0,100):
-			if flag and chairs[i] == 1:
-				chairs[i] = 0
-				flag = False
-				continue
-			elif (not flag) and chairs[i] == 1:
-				flag = True
-			else:
-				continue
-		if chairs.count(1) == 1:
-			break
-	return chairs.index(1)
+    chairs = []
+    for i in range(0, N):
+        chairs.append(1)
+    flag = True
+    while True:
+        for i in range(0, N):
+            if flag and chairs[i] == 1:
+                chairs[i] = 0
+                flag = False
+                continue
+            elif (not flag) and chairs[i] == 1:
+                flag = True
+            else:
+                continue
+        if chairs.count(1) == 1:
+            break
+    return chairs.index(1)
 
-print ChairsSurvival(100)
+# print ChairsSurvival(1000000)
+
+import time
+elapseTime = []
+
+for i in range(10000, 10001):
+    # print i
+    bgnTime = time.clock()
+    print ChairsSurvival(i)
+    endTime = time.clock()
+    elapseTime.append(endTime - bgnTime)
+
+
+print elapseTime

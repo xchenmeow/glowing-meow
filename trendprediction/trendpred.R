f573ac3f8157821736a04cecb58cc7da497204a1
timmyshen <benqingshen@gmail.com>
2013-10-14 14:45:43 -0500

    indenting r code

--- trendprediction/trendpred.R
+++ trendprediction/trendpred.R
@@ -10,33 +10,33 @@ inputfile=read.csv(file = "C:/Users/cr/Documents/SSE300Data.csv", head = FALSE)
 
 trendpred <- function(n)
 {
-# assigning...
-open <- inputfile[,2+(i-1)*9]
-open <- as.numeric(as.character(open))
-open <- open[4:length(open)]
-close <- inputfile[,5+(i-1)*9]
-close <- as.numeric(as.character(close))
-close <- close[4:length(close)]
+  # assigning...
+  open <- inputfile[,2+(i-1)*9]
+  open <- as.numeric(as.character(open))
+  open <- open[4:length(open)]
+  close <- inputfile[,5+(i-1)*9]
+  close <- as.numeric(as.character(close))
+  close <- close[4:length(close)]
 
-# --------------------
-# calculating...
-ret <- log(close) - log(open)
-# flag classify all the returns to 10 class
-flag <- floor(ret*50)
-flag[flag < -4] <- -4
-# buy one share when the stock rise at time t-1
-position <- c(0, flag[1:length(flag)-1])
-position[position < 0] <- 0
-deltavalue <- close - open
-# classvalue is the total return in a specific class
-classrisk <- rep(0,each = 5)
-for (i in 1:5)
-{
-	classvalue[i] <- sum(deltavalue[position == i])
-	classrisk[i] <- sd(deltavalue[position == i])
-}
-temp = c(classvalue, classrisk)
-return (matrix(temp,nrow = 5, ncol = 2))
+  # --------------------
+  # calculating...
+  ret <- log(close) - log(open)
+  # flag classify all the returns to 10 class
+  flag <- floor(ret*50)
+  flag[flag < -4] <- -4
+  # buy one share when the stock rise at time t-1
+  position <- c(0, flag[1:length(flag)-1])
+  position[position < 0] <- 0
+  deltavalue <- close - open
+  # classvalue is the total return in a specific class
+  classrisk <- rep(0,each = 5)
+  for (i in 1:5)
+  {
+  	classvalue[i] <- sum(deltavalue[position == i])
+  	classrisk[i] <- sd(deltavalue[position == i])
+  }
+  temp = c(classvalue, classrisk)
+  return (matrix(temp,nrow = 5, ncol = 2))
 }
 
 #----------------------

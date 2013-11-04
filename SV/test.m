48c60a89842d5a497b54ea90df30647218adb495
xchenmeow <chenxy12345@yahoo.com>
2013-11-04 17:11:55 +0800

    stochastic volatility model calibration not finished...

---
+++ SV/test.m
@@ -0,0 +1,8 @@
+
+price = xlsread('SSE.xlsx',2);
+ret = price2ret(flipud(price(:,6)));
+ret = ret(end-100:end);
+lb = [-10, 0.2, 0.01, -0.99];
+ub = [10, 0.99, 1, 0.99];
+theta0 = [-0.3, 0.95, 0.2, 0.1];
+[theta, fval, exitflag] = fminsearch(@(theta) SVCalibration(ret, theta), theta0);

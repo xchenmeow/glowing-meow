76bf7547b54b0abebccc2a845f49266e897ba0a1
xchenmeow <chenxy12345@yahoo.com>
2013-11-05 16:17:21 +0800

    still working on SV......

--- SV/SVCalibration.m
+++ SV/SVCalibration.m
@@ -1,29 +1,34 @@
 function mlikelihood = SVCalibration(ret, theta)
+% reference: Nonlinear Filtering for Stochastic Volatiliy Models with Heavy
+%   Tails and Leverage by AE Clements, SI White.
+% Calibrate log-AR(1) model. 
+% input: return time series(last is the latest), parameters to calibrate
+% output: minus likelihood function
+
 %%
 delta = 0.01;
 x = delta:delta:0.5;
-% theta = [-0.3, 0.95, 0.2, 0.1];
 alpha = theta(1);
 beta = theta(2);
 sigmav = theta(3);
 rho = theta(4);
-x0 = 0.2;
+x0 = log(0.2^2);
 xi = repmat(x',1,length(x));
 xj = repmat(x,length(x),1);
 
 %%
 yt = ret(1);
-u0 = exp(-(xi-alpha-beta*x0)^2/2/sigmav^2) / sqrt(2*pi*sigmav^2);
-q = delta * exp(-(xi-alpha-beta*xj)^2/2/sigmav^2) / sqrt(2*pi*sigmav^2);
+u0 = exp(-(xi-alpha-beta*x0).^2/2/sigmav^2) / sqrt(2*pi*sigmav^2);
+q = exp(-(xi-alpha-beta*xj).^2/2/sigmav^2) / sqrt(2*pi*sigmav^2);
 mu = exp(xi/2) .* rho .* (xi-alpha-beta*xj)/sigmav;
-pt = q * u0;
+pt = q .* u0;
 rt = exp(-(yt - mu).^2./(2*exp(xi)*(1-rho^2))) ./ sqrt(2*pi*exp(xi)*(1-rho^2));
 for i = 2:length(ret)
     yt = ret(i);
-    pt = q .* repmat(sum(rt.*pt),length(x),1) / sum(sum(rt.*pt));
+    pt = q .* repmat(sum(rt.*pt,2),1,length(x))*delta / sum(sum(rt.*pt))/delta^2;
     rt = exp(-(yt - mu).^2./(2*exp(xi)*(1-rho^2))) ./ sqrt(2*pi*exp(xi)*(1-rho^2));
 end
 
 
-mlikelihood = -sum(sum(rt*pt));
+mlikelihood = -sum(sum(rt*pt))*delta^2;
 
--- SV/test.m
+++ SV/test.m
@@ -4,5 +4,5 @@ ret = price2ret(flipud(price(:,6)));
 ret = ret(end-100:end);
 lb = [-10, 0.2, 0.01, -0.99];
 ub = [10, 0.99, 1, 0.99];
-theta0 = [-0.3, 0.95, 0.2, 0.1];
+theta0 = [-0.01, 0.95, 0.2, 0.1];
 [theta, fval, exitflag] = fminsearch(@(theta) SVCalibration(ret, theta), theta0);

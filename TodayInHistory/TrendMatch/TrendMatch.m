6901a9bd547d0031703311f45c14826c5e2ffdea
glowingmeow <chenxy12345@yahoo.com>
2013-11-27 15:22:13 +0800

    add a closeflag to call SetTrend with or without previous close.

--- TodayInHistory/TrendMatch/TrendMatch.m
+++ TodayInHistory/TrendMatch/TrendMatch.m
@@ -27,17 +27,23 @@ close = cell2mat(price');
 close = close';
 
 k = 10;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% trend = SetTrend(k, close); 
-% set trend without the previous close
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-trend = SetTrendwithClose(k, close, close(:,end)); 
-% set trend with the previous close
+%%%%%%%%%%%%%%%%%%
+closeflag = 0;
+% 0 set trend without the previous close
+% 1 set trend with the previous close
+%%%%%%%%%%%%%%%%%%
+if closeflag == 0
+    trend = SetTrend(k, close); 
+elseif closeflag == 1
+    trend = SetTrendwithClose(k, close, close(:,end)); 
+else
+    fprintf('closeflag can only take 0 or 1');
+end
 
 %%
 % finding the most similar trend
 samplesize = 1000;
-trainingsample = trend(:,1:k+1);
+trainingsample = trend(:,1:k+closeflag);
 regsample = Reg(trainingsample);
 
 trenddist = zeros(size(trainingsample,1) - samplesize,1);
@@ -51,7 +57,7 @@ end
 
 %%
 % backtesting...
-testingsample = trend(:,k+2:end);
+testingsample = trend(:,k+1+closeflag:end);
 error = zeros(length(idx),1);
 for i = 1:length(idx)
     error(i) = pdist2(testingsample(idx(i),:), testingsample(samplesize+i,:));
@@ -90,32 +96,37 @@ end
 
 % load today's morning data
 % should change the way of loading data
-todaydata = xlsread('nov1113highfreq.xlsx');
-previousclose = 1.64;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% datatoday = SetTrend(k/2, todaydata(:,4)'); 
-% set trend without previous close
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-datatoday = SetTrendwithClose(k/2, todaydata(:,4)', previousclose);
-% set trend with previous close
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+todaydata = xlsread('nov27highfreq.xlsx');
+previousclose = 1.63;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if closeflag == 0
+    datatoday = SetTrend(k/2, todaydata(:,4)'); 
+elseif closeflag == 1
+    datatoday = SetTrendwithClose(k/2, todaydata(:,4)', previousclose);
+end
 regtoday = Reg(datatoday);
 [trenddisttoday, idxtoday] = min(pdist2(regsample(1:end-1,:),regtoday));
-regtestsample = Reg(testingsample(idxtoday,:));
+minsample = min(trainingsample(idxtoday,:));
+maxsample = max(trainingsample(idxtoday,:));
+regtestsample = (testingsample(idxtoday,:)-minsample)/(maxsample-minsample);
 predictingtrend = regtestsample * (max(datatoday)-min(datatoday)) ...
     + min(datatoday);
+
 
 % plot the prediction
 subplot(2,1,1);
 hold on
-plot(1:k+1,trainingsample(idxtoday,:),'r');
-plot(k+2:2*k+2,testingsample(idxtoday,:),'b');
+plot(1:k+closeflag,trainingsample(idxtoday,:),'r');
+plot(k+closeflag:2*k+2*closeflag,[trainingsample(idxtoday,end),testingsample(idxtoday,:)],'b');
 xlabel(datenum(idxtoday));
 hold off
 
 subplot(2,1,2);
 hold on
-plot(1:k+1,datatoday,'r');
-plot(k+2:2*k+2,predictingtrend, 'b--');
+plot(1:k+closeflag,datatoday,'r');
+plot(k+closeflag:2*k+2*closeflag,[datatoday(end), predictingtrend], 'b--');
 xlabel(date);
 hold off
 

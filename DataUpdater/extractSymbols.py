7f078f97936f153fb0f3088ef45414465c53db99
Benqing Shen <benqingshen@gmail.com>
2014-02-09 21:30:11 -0600

    little fix in csv output

--- DataUpdater/extractSymbols.py
+++ DataUpdater/extractSymbols.py
@@ -9,4 +9,4 @@ with open(resource, 'rb') as csvfile, open('symbols.csv', 'wb') as symbol_file:
 	 # help here.
 	 symbols_writer = csv.writer(symbol_file)
 	 for row in symbols_reader:
-	 	symbols_writer.writerow(row[1])
+	 	symbols_writer.writerow([row[1]])

9b74d0c31ef58be326d744a36da7c969c18fc941
Benqing Shen <benqingshen@gmail.com>
2014-02-14 10:45:50 -0600

    automate vol estimate

--- Volatility/estimate.R
+++ Volatility/estimate.R
@@ -1,14 +1,30 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-data_file <- args[1]
+# args <- c("C:\\Users\\benqing.shen\\Desktop\\Data", "HYG_returns")
+
+data_dir <- args[1]
+
+# data_file: assumes Yahoo return time series.
+# assumes also date range is processed
+data_name <- args[2]
+
+data_file <- paste(data_dir, "\\", data_name, ".csv", sep="")
+
+# load spec
+if(length(args) == 3) {
+  load(args[3])
+} else {
+  load("ModelSpec.RData")
+}
 
 df <- read.csv(data_file)
 # rownames(df) <- df$Date
 
 library(rugarch)
-spec <- ugarchspec(mean.model=list(armaOrder=c(0,0), include.mean=FALSE))
+# spec <- ugarchspec(mean.model=list(armaOrder=c(0,0), include.mean=FALSE))
 fit <- ugarchfit(spec=spec, data=df[,2])
-rugarch::show(fit)
+save(fit, file=paste(data_name, "fit.RData", sep='_'))
+# rugarch::show(fit)
 # ugarchforecast(fitORspec=fit, data=df[,2], n.ahead=1)
 
 
--- Volatility/run.bat
+++ Volatility/run.bat
@@ -1,1 +1,2 @@
-C:\Users\benqing.shen\Documents\R\R-3.0.2\bin\Rscript.exe estimate.R C:\\Users\\benqing.shen\\Desktop\\Data\\HYG_returns.csv > output.txt
+# C:\Users\benqing.shen\Documents\R\R-3.0.2\bin\Rscript.exe estimate.R C:\\Users\\benqing.shen\\Desktop\\Data\\HYG_returns.csv > output.txt
+C:\Users\benqing.shen\Documents\R\R-3.0.2\bin\Rscript.exe estimate.R C:\\Users\\benqing.shen\\Desktop\\Data HYG_returns

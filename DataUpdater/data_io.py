9ed34f693987ad4d57516283f9394cdeeb109e2f
Benqing Shen <benqingshen@gmail.com>
2014-02-19 16:48:42 -0600

    year month date value

---
+++ DataUpdater/data_io.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+def from_ymdv_csv(path):
+	return pd.DataFrame.from_csv(path, header=0, index_col=[0, 1, 2], parse_dates=False)
+
+
+def ymdv_2_monthly_return(df, months, return_type='simple'):
+	if return_type != 'simple':
+		raise NotImplemented
+	gdf = df.groupby(level=['Year', 'Month'])
+	ymv = gdf.last()
+	month_end_levels = ymv.tail(months + 1)
+	monthly_returns = month_end_levels.pct_change()
+	return monthly_returns[1:]
